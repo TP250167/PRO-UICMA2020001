4050e4d89514dbc46ec19409bd3a18f4cf46fb35
KOSOFT\sathishkumar.m <sathishkumar.m@kosoft.co>
2020-03-18 12:13:35 +0530

    #7854 Cliams from ui change

--- UI/src/app/@models/claims/claim-model.ts
+++ UI/src/app/@models/claims/claim-model.ts
@@ -1,52 +1,51 @@
-
 export class ClaimModel {
-    id                     : number
-    requestNumber          : number
-    type                   : string
-    requestTitle           : string
-    claimantName           : string
-    currentStage           : string
-    createdOn              : string
-    modifiedOn             : string
-    gender                 : string
-    employeeNumber         : string
-    socialSecurityNumber   : string
-    createdBy              : string
-    modifiedBy             : string
-    reasonForSeparation    : string
-    newClaim               : string
-    additionalClaim        : string
-    benefitYearBeginning   : number
-    dateMailedToEDD        : string
-    mailDate               : string
-    effectiveDateOfClaim   : string
-    lastDateWorked         : string
-    address                : string
-    protestDecision        : string
-    receivedDate           : string
-    deadlineDate           : string
-    claimantStatus         : string
-    byb                    : string
-    city                   : string
-    state                  : string
-    zipcode                : string
-    claimResponse          : string
-    claimInterview         : string
-    claimDetermination     : string
-    responsetoEmployer     : string
-    claimAppeal            : string
-    alJdecision            : string
-    acknowledgement        : string
-    wagesAfterAppeal       : string
-    additionalInformation  : string
-    hearing                : string
-    benefitAudit           : string
-    writ                   : string
-    requestToReopen        : string
-    benefitCharge          : string
-    determinationAppeal    : string
-    wages                  : string
-    wagesAppeal            : string
-    requestForEmployeeData : string
-    cuiaBboardAppeal       : string
+  id: number;
+  requestNumber: number;
+  type: string;
+  requestTitle: string;
+  claimantName: string;
+  currentStage: string;
+  createdOn: string;
+  modifiedOn: string;
+  gender: string;
+  employeeNumber: string;
+  socialSecurityNumber: string;
+  createdBy: string;
+  modifiedBy: string;
+  reasonForSeparation: string;
+  newClaim: string;
+  additionalClaim: string;
+  benefitYearBeginning: number;
+  dateMailedToEDD: string;
+  mailDate: string;
+  effectiveDateOfClaim: string;
+  lastDateWorked: string;
+  address: string;
+  protestDecision: string;
+  receivedDate: string;
+  deadlineDate: string;
+  claimantStatus: string;
+  byb: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  claimResponse: string;
+  claimInterview: string;
+  claimDetermination: string;
+  responsetoEmployer: string;
+  claimAppeal: string;
+  alJdecision: string;
+  acknowledgement: string;
+  wagesAfterAppeal: string;
+  additionalInformation: string;
+  hearing: string;
+  benefitAudit: string;
+  writ: string;
+  requestToReopen: string;
+  benefitCharge: string;
+  determinationAppeal: string;
+  wages: string;
+  wagesAppeal: string;
+  requestForEmployeeData: string;
+  cuiaBboardAppeal: string;
 }
